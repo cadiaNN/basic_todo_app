8ef6218e4255679d664f70b8f8f47fff24ec4605
nabievnasibullo <nasibullo0731@gmail.com>
2023-12-13 23:06:10 +0100

    Initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+List <Color> backgroundColors = [
+const Color(0xffCCE5FF), // light_blue
+const Color(0xffD7F9E9), // pale_green
+const Color(0xffFFF9e1), // pale_yellow
+const Color(0xffF5E6CC), // beige
+const Color(0xffFFD6D6), // light_pink
+const Color(0xffE5E5E5), // light_grey
+const Color(0xffFFF0F0), // pale_pink
+const Color(0xffE6F9FF), // pale_blue
+const Color(0xffD4EDDA), // pale_green
+const Color(0xffFFF3CD), // pale_orange
+];
